d50a94f2014179eb881570c5790f8060518a6a2d
Sean Wong <sean@seedvector.com>
2025-11-20 01:46:39 +0800

    chore: remove enableMapSet and update .gitignore
    - Remove enableMapSet() import and call from main.tsx (no longer needed with Record)
    - Update .gitignore: change temp to docs directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { enableMapSet } from "immer";
 import App from "./App";
-
-// Enable Immer MapSet plugin for stores using Map/Set
-enableMapSet();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
